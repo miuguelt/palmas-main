7a2925aa4f5f66efe7ad43d516e9272e9eaba457
miuguelt <miuguelt@gmail.com>
2025-05-20 11:55:42 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,10 @@ set -e
 APP_DATA_DIR="/2826869/alejandro"      # Archivos iniciales en el contenedor
 VOLUME_DIR="/app/app/static"  # Volumen montado desde el host
 
-# Crea directorio en el contenedor si no existe
-mkdir -p "$APP_DATA_DIR"
-
-# Si el volumen está vacío, copia todo desde el contenedor
-if [ ! -d "$VOLUME_DIR" ] || [ "$(ls -A "$VOLUME_DIR")" = "" ]; then
-    echo "Volumen vacío. Copiando archivos iniciales del contenedor..."
-    cp -r "$APP_DATA_DIR"/. "$VOLUME_DIR/"
+# Verifica si el volumen está vacío
+if [ -d "$VOLUME_DIR" ] && [ "$(ls -A $VOLUME_DIR)" ]; then
+    echo "El volumen ya tiene datos. No se copian archivos iniciales."
 else
-    echo "Volumen no vacío. Fusionando archivos nuevos del contenedor..."
-    
-    # Usa rsync para copiar solo archivos nuevos o modificados (sin sobreescribir)
-    rsync -a --ignore-existing "$APP_DATA_DIR"/ "$VOLUME_DIR"/
+    echo "El volumen está vacío. Copiando archivos iniciales..."
+    cp -r "$APP_DATA_DIR"/. "$VOLUME_DIR"
 fi
